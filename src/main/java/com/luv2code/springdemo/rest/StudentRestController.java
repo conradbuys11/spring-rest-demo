c8d4adb3bc01914ed8574fe8b9146bf7d323efb9
conradbuys11 <conradbuys11@gmail.com>
2022-03-04 14:58:43 -0500

    exception handling demo

--- src/main/java/com/luv2code/springdemo/rest/StudentRestController.java
+++ src/main/java/com/luv2code/springdemo/rest/StudentRestController.java
@@ -3,7 +3,9 @@ package com.luv2code.springdemo.rest;
 import java.util.ArrayList;
 import java.util.List;
 
+import javax.annotation.PostConstruct;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -12,15 +14,27 @@ import com.luv2code.springdemo.entity.Student;
 @RestController
 @RequestMapping("/api")
 public class StudentRestController {
-
-	@GetMapping("/students")
-	public List<Student> getStudents() {
-		List<Student> students = new ArrayList<>();
+	
+	private List<Student> students;
+	
+	@PostConstruct
+	public void loadData() {
+		students = new ArrayList<>();
 		students.add(new Student("Poornima", "Patel"));
 		students.add(new Student("Mario", "Rossi"));
 		students.add(new Student("Mary", "Smith"));
-		
+	}
+
+	@GetMapping("/students")
+	public List<Student> getStudents() {
 		return students;
 	}
 	
+	@GetMapping("/students/{studentId}")
+	public Student getStudent(@PathVariable int studentId) {
+		if((studentId >= students.size()) || (studentId < 0)) {
+			throw new StudentNotFoundException("Student id not found - " + studentId);
+		}
+		return students.get(studentId);
+	}
 }
